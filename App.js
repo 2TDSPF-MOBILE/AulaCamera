58ce39af2f04c3aa2401452dbb73a459eba7ad7d
Prof. Fernando Pinéo <pf1963@fiap.com.br>
2025-04-25 09:49:46 -0300

    Realizado o app de acesso a câmera e salvamento na galeria

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+
+//Novo sistema de câmeras no Expo SDK 51+
+import { CameraView, useCameraPermissions } from 'expo-camera';
+
+//Biblioteca para salvar a foto na galeria
+import * as MediaLibrary from 'expo-media-library'
 
 export default function App() {
+  //Estado para permissão da camera
+  const [permissaoCam, requestPermissaoCam] = useCameraPermissions()
+
+  //Estado para permissão da galeria
+  const [permissaoMedia, requestPermissaoMedia] = MediaLibrary.usePermissions()
+
+  //Referência da câmera (acesso direto ao componente)
+  const cameraRef = useRef(null)
+
+  //Estado para a foto capturado
+  const [foto, setFoto] = useState(null)
+
+  //Solicitar permissão para acessar a galeria no inicio do app
+  useEffect(() => {
+    if (permissaoMedia === null) return;
+    if (!permissaoMedia?.granted) {
+      requestPermissaoMedia()
+    }
+  }, [])
+
+  //Função para tirar foto
+  const tirarFoto = async () => {
+    if (cameraRef.current) {
+      const dadoFoto = await cameraRef.current.takePictureAsync(); //captura foto
+      setFoto(dadoFoto)//Salva Estado
+    }
+  }
+
+  const salvarFoto = async () => {
+    try {
+      await MediaLibrary.createAssetAsync(foto.uri)//Salvar a foto na galeria
+      Alert.alert("Sucesso", "Foto salva com suceso!")
+      setFoto(null) //Reseta o estado para eu tirar uma foto
+    } catch (err) {
+      Alert.alert("Error", "Error ao Salvar Foto.")
+    }
+  }
+
+  //Enquanto a permissão não estiver carregada
+  if (!permissaoCam) return <View />
+
+  if (!permissaoCam.granted) {
+    return (
+      <View>
+        <Text>Permissão da câmera não foi concedida</Text>
+        <Button title='Permitir' onPress={requestPermissaoCam} />
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {!foto?(
+        <>
+          <CameraView
+            ref={cameraRef}
+            style={styles.camera}
+            facing='back'
+          />
+          <Button title='Titar Foto' onPress={tirarFoto} />
+        </>
+      ):(
+        <>
+          <Image source={{uri:foto.uri}} style={styles.preview}/>
+          <Button title='Tirar nova foto' onPress={()=>setFoto(null)}/>
+          <Button title='Salvar Foto' onPress={salvarFoto} />
+        </>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  camera: {
+    flex: 1
+  },
+  preview:{
+    flex:1
+  }
 });
